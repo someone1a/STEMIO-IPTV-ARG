d89f8dd4efa1c2896b12e6536ab14df897ac5e07
Walter Carrasco <carrascowalter440@gmail.com>
2026-01-11 19:02:54 -0300

    Empezar de 0

--- server.js
+++
@@ -1,8 +0,0 @@
-// Bootstrap mínimo: carga el servidor modular desde src/
-require('dotenv').config();
-
-// Arranca el servicio principal (src/server.js) que registra el addon y expone endpoints
-require('./src/server');
-
-// Export para testing o uso avanzado
-module.exports = {};
